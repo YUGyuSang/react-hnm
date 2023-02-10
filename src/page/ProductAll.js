e933c7961894942a7f8ff2f164e29f71f4a32897
gyusang <monet000414@naver.com>
2023-02-10 18:57:21 +0900

    product Card

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from './ProductCard';
 
 const ProductAll = () => {
+    const [productList, setProudctList] = useState([]); // ui로 보여주기 위해
+
+const getProducts= async () =>{
+    let url = `http://localhost:3004/products`
+    let response = await fetch(url);
+    let data = await response.json();
+    setProudctList(data)
+}
+    useEffect(()=>{
+        getProducts()
+    },[])
   return (
     <div>
-      전체상품페이지.
+      <ProductCard />
     </div>
   )
 }
---
+++ src/page/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src='https://lp2.hm.com/hmgoepprod?set=source[/d4/ee/d4eef0a138cb6811f6cd771121c7314e0626bfd8.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]'/>
+      <div>Conscious choice</div>
+      <div>벨티드 트윌 코드</div>
+      <div>$4000</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
